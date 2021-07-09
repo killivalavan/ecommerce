d1b493d8a60b319d5db9113729c5c82e93365847
killivalavan <killivalavan.exe@gmail.com>
2021-07-09 17:58:55 +0530

    initial

--- src/Components/Products/Product.js
+++ src/Components/Products/Product.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import cart from '../../img/cart.svg';
 
 
